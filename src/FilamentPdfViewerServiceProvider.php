b66a18481f23776f38cf2554e0f056bd41872a17
Rômulo Ramos <rmsramos@gmail.com>
2024-06-22 00:28:25 -0300

    remove unnecessary files

--- src/FilamentPdfViewerServiceProvider.php
+++ src/FilamentPdfViewerServiceProvider.php
@@ -2,13 +2,6 @@
 
 namespace Joaopaulolndev\FilamentPdfViewer;
 
-use Filament\Support\Assets\AlpineComponent;
-use Filament\Support\Assets\Asset;
-use Filament\Support\Assets\Css;
-use Filament\Support\Assets\Js;
-use Filament\Support\Facades\FilamentAsset;
-use Filament\Support\Facades\FilamentIcon;
-use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
@@ -34,7 +27,6 @@ class FilamentPdfViewerServiceProvider extends PackageServiceProvider
             ->hasInstallCommand(function (InstallCommand $command) {
                 $command
                     ->publishConfigFile()
-                    ->publishMigrations()
                     ->askToRunMigrations()
                     ->askToStarRepoOnGitHub('joaopaulolndev/filament-pdf-viewer');
             });
@@ -43,10 +35,6 @@ class FilamentPdfViewerServiceProvider extends PackageServiceProvider
 
         if (file_exists($package->basePath("/../config/{$configFileName}.php"))) {
             $package->hasConfigFile();
-        }
-
-        if (file_exists($package->basePath('/../database/migrations'))) {
-            $package->hasMigrations($this->getMigrations());
         }
 
         if (file_exists($package->basePath('/../resources/lang'))) {
@@ -64,29 +52,6 @@ class FilamentPdfViewerServiceProvider extends PackageServiceProvider
 
     public function packageBooted(): void
     {
-        // Asset Registration
-//        FilamentAsset::register(
-//            $this->getAssets(),
-//            $this->getAssetPackageName()
-//        );
-
-        FilamentAsset::registerScriptData(
-            $this->getScriptData(),
-            $this->getAssetPackageName()
-        );
-
-        // Icon Registration
-        FilamentIcon::register($this->getIcons());
-
-        // Handle Stubs
-        if (app()->runningInConsole()) {
-            foreach (app(Filesystem::class)->files(__DIR__ . '/../stubs/') as $file) {
-                $this->publishes([
-                    $file->getRealPath() => base_path("stubs/filament-pdf-viewer/{$file->getFilename()}"),
-                ], 'filament-pdf-viewer-stubs');
-            }
-        }
-
         // Testing
         Testable::mixin(new TestsFilamentPdfViewer());
     }
@@ -97,58 +62,12 @@ class FilamentPdfViewerServiceProvider extends PackageServiceProvider
     }
 
     /**
-     * @return array<Asset>
-     */
-    protected function getAssets(): array
-    {
-        return [
-            // AlpineComponent::make('filament-pdf-viewer', __DIR__ . '/../resources/dist/components/filament-pdf-viewer.js'),
-            Css::make('filament-pdf-viewer-styles', __DIR__ . '/../resources/dist/filament-pdf-viewer.css'),
-            Js::make('filament-pdf-viewer-scripts', __DIR__ . '/../resources/dist/filament-pdf-viewer.js'),
-        ];
-    }
-
-    /**
      * @return array<class-string>
      */
     protected function getCommands(): array
     {
         return [
             FilamentPdfViewerCommand::class,
-        ];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getIcons(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getRoutes(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string, mixed>
-     */
-    protected function getScriptData(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getMigrations(): array
-    {
-        return [
-            'create_filament-pdf-viewer_table',
         ];
     }
 }
